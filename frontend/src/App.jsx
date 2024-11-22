9491881272f8feeffa0409c53c9654b9f290a50d
Karthikeya Reddy Ariveeti <karthikeya.reddy@tigeranalytics.local>
2024-11-22 10:45:35 +0530

    dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 // import AddProject from "@/components/custom/AddProject"
 import ProjectPage from "@/pages/ProjectPage/ProjectPage"
 import ErrorPage from "@/pages/ErrorPage/ErrorPage"
+import Dashboard from "@/pages/Dashboard/Dashboard";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/:id" element={<ProjectPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
---
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import Projects from "@/components/custom/Projects";
+
+const Dashboard = () =>{
+    return(
+        <div>
+            This is Dashboard
+            <Projects />
+        </div>
+    )
+}
+
+export default Dashboard;
