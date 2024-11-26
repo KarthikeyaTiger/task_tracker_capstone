77c32ee0332a83904e709bb5b93483bf082c90d3
krishnasashank256 <krishnasashank.a@tigeranalytics.com>
2024-11-26 12:17:14 +0530

    basic sso

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="695958764159-rbdkroesu7t6s5jf5935cd77ausfmadt.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,38 +1,12 @@
-import React from 'react';
-import { GoogleLogin } from 'react-google-login';
-import axios from 'axios';
-
-const CLIENT_ID = "695958764159-rbdkroesu7t6s5jf5935cd77ausfmadt.apps.googleusercontent.com";
+import Login from '@/components/common/Login'
+import React from 'react'
 
 const LoginPage = () => {
-  const handleLoginSuccess = async (response) => {
-    const { tokenId } = response;
-
-    try {
-      // Send the tokenId to your FastAPI backend for verification
-      const res = await axios.post('http://127.0.0.1:8000/verify-token', { id_token: tokenId });
-      console.log('User info from backend:', res.data);
-    } catch (error) {
-      console.error("Error verifying token:", error);
-    }
-  };
-
-  const handleLoginFailure = (error) => {
-    console.error("Google login failed:", error);
-  };
-
   return (
     <div>
-      <h2>Sign in with Google</h2>
-      <GoogleLogin
-        clientId={CLIENT_ID}
-        buttonText="Login with Google"
-        onSuccess={handleLoginSuccess}
-        onFailure={handleLoginFailure}
-        cookiePolicy="single_host_origin"
-      />
+      <Login />
     </div>
-  );
-};
+  )
+}
 
-export default LoginPage;
+export default LoginPage
